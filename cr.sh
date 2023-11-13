e22008bd7016583383cc4d87ac3f752872cd0d5b
Stefan Krenz <krenz.stefan@googlemail.com>
2023-11-13 12:14:40 +0100

    fix: Revert commit

--- cr.sh
+++ cr.sh
@@ -3,7 +3,6 @@
 DEFAULT_CHART_RELEASER_VERSION=v1.4.0
 
 arch=$(uname -m)
-os=$(uname -s | tr '[:upper:]' '[:lower:]')
 
 version="$DEFAULT_CHART_RELEASER_VERSION"
 cache_dir="$RUNNER_TOOL_CACHE/ct/$version/$arch"
@@ -11,7 +10,7 @@ if [[ ! -d "$cache_dir" ]]; then
   mkdir -p "$cache_dir"
 
   echo "Installing chart-releaser..."
-  curl -sSLo cr.tar.gz "https://github.com/helm/chart-releaser/releases/download/$version/chart-releaser_${version#v}_${os}_${arch}.tar.gz"
+  curl -sSLo cr.tar.gz "https://github.com/helm/chart-releaser/releases/download/$version/chart-releaser_${version#v}_linux_amd64.tar.gz"
   tar -xzf cr.tar.gz -C "$cache_dir"
   rm -f cr.tar.gz
 
